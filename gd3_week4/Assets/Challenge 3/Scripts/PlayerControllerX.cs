f43146fe13b90cbbc7056f71b012179fbf3a8080
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2024-08-27 08:44:33 +0100

    Week 4 final Commit

--- gd3_week4/Assets/Challenge 3/Scripts/PlayerControllerX.cs
+++ gd3_week4/Assets/Challenge 3/Scripts/PlayerControllerX.cs
@@ -23,9 +23,12 @@ public class PlayerControllerX : MonoBehaviour
     {
         Physics.gravity *= gravityModifier;
         playerAudio = GetComponent<AudioSource>();
+        playerRb = GetComponent<Rigidbody>();
 
         // Apply a small upward force at the start of the game
         playerRb.AddForce(Vector3.up * 5, ForceMode.Impulse);
+
+        
 
     }
 
